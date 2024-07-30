0b65a4553c4ef52c8ecd20ddb77692b567630c1e
myra-everwatch <myra.cropper@everwatchsolutions.com>
2024-07-30 13:36:35 +0000

    Data

--- yolo_code/yolo_model.py
+++ yolo_code/yolo_model.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import boto3
 
 model = YOLO("yolov8n.yaml")
 
@@ -11,17 +12,21 @@ map75 = metrics.box.map75
 maps = metrics.box.maps
 
 
-metrics_df = pd.DataFrame({
-    'mAP50-95': [map50_95],
-    'mAP50': [map50],
-    'mAP75': [map75],
-    'mAPs': [maps]
-})
-
-
-
-
 #results = model("")
 
 # Export the model to ONNX format
 path = model.export(format="onnx")
+#save model to s3 bucket 
+
+bucket_name="spongebobpipeline"
+role_arn = 'arn:aws:iam::533267059960:role/aws-s3-access'
+session_name = 'kubeflow-pipeline-session'
+sts_client = boto3.client('sts')
+response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=session_name)
+credentials = response['Credentials']
+    
+#Configure AWS SDK with temporary credentials
+s3_client = boto3.client('s3',
+                      aws_access_key_id=credentials['AccessKeyId'],
+                      aws_secret_access_key=credentials['SecretAccessKey'],
+                      aws_session_token=credentials['SessionToken'])
